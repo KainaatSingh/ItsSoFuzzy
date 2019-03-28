98d037b75876959e3258834c50d24773ae68cf38
singh <s6kasing@uni-bonn.de>
2019-03-28 05:25:20 +0100

    Replace BitFlip.py

--- testcases/BitFlip.py
+++ testcases/BitFlip.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-from random import randrange, randint
+from random import randrange, randint, choice
 from sys import path
 path.append('../seed/')
 from GenerateSeed import *
@@ -13,42 +13,60 @@ class BitFlip():
     def select_seed(self, option):
         
         switcher = {
-                'random_printable': GenerateSeed.random_printable_seed,
-                'random': GenerateSeed.random_seed,
-                'exhaustive': GenerateSeed.exhaustive_seed
+                'random_printable': random_printable_seed,
+                'random': random_seed,
+                'exhaustive': exhaustive_seed
                 }
         func = switcher.get(option)
         return func
     
     
-    def flip_one(length, option):
+    def flip_one(length, option, string):
+        #print('one flip')
         obj = BitFlip()
+        
+        if option == 'none':
+            mutated_inputstring = ""
+            input_list = []
+            
+            for letter in string:
+                input_list.append(letter)
+            
+            index = randrange(len(input_list))
+            bit = 1 << randrange(8)
+            input_list[index] = chr(ord(input_list[index]) ^ bit)
+            
+            mutated_inputstring = ''.join(input_list)
+           
+            return mutated_inputstring
 
-        seed_func = obj.select_seed(option)
-        input_string = ""
-        input_string = seed_func(length)
+        else:    
+            #print('flip_one')
+            seed_func = obj.select_seed(option)
+            input_string = ""
+            input_string = seed_func(length)
 
-        input_list = []
-        for letter in input_string:
-            input_list.append(letter)
+            input_list = []
+            for letter in input_string:
+                input_list.append(letter)
 
-        mutated_inputlist = []
-        mutated_inputlist.append(input_string)
-        for i in range(0, len(input_list)):
-            temp_character = input_list[i]
+            mutated_inputlist = []
+            mutated_inputlist.append(input_string)
+            for i in range(0, len(input_list)):
+                temp_character = input_list[i]
                 
-            bit = 1 << randint(0, 8)
-            new_character = chr(ord(temp_character) ^ bit)
-            input_list[i] = new_character
+                bit = 1 << randint(0, 8)
+                new_character = chr(ord(temp_character) ^ bit)
+                input_list[i] = new_character
                 
-            mutated_string = ''.join(input_list)
-            mutated_inputlist.append(mutated_string)
-            mutated_string = ""
+                mutated_string = ''.join(input_list)
+                mutated_inputlist.append(mutated_string)
+                mutated_string = ""
        
-        return mutated_inputlist
+            return mutated_inputlist
 
     
-    def flip_one_restore(length, option):
+    def flip_one_restore(length, option, string):
         obj = BitFlip()
 
         seed_func = obj.select_seed(option)
@@ -78,24 +96,41 @@ class BitFlip():
         return mutated_inputlist
 
     
-    def flip_all(length, option):
+    def flip_all(length, option, string):
+        #print('flip all')
         obj = BitFlip()
 
-        seed_func = obj.select_seed(option)
+        if option == 'none':
+            mutated_inputstring = ""
+            input_list = []
+            for letter in string:
+                input_list.append(letter)
 
-        input_string = ""
-        input_string = seed_func(length)
+            for i in range(0, len(input_list)):
+                for j in range(0,8):
+                    bit = 1 << j
+                    input_list[i] = chr(ord(input_list[i]) ^ bit)
+            
+            mutated_inputstring  = ''.join(input_list)
+            
+            return mutated_inputstring
 
-        input_list = []
-        for letter in input_string:
-            input_list.append(letter)
+        else:
+            seed_func = obj.select_seed(option)
 
-        mutated_inputlist = []
-        temp_list = []
-        mutated_inputlist.append(input_string)
-        for i in range(0, len(input_list)):
-            temp_list[:] = input_list
-            temp_character = temp_list[i]
+            input_string = ""
+            input_string = seed_func(length)
+
+            input_list = []
+            for letter in input_string:
+                input_list.append(letter)
+
+            mutated_inputlist = []
+            temp_list = []
+            mutated_inputlist.append(input_string)
+            for i in range(0, len(input_list)):
+                temp_list[:] = input_list
+                temp_character = temp_list[i]
             
             for j in range(0,8):   
                 bit = 1 << j
@@ -106,46 +141,68 @@ class BitFlip():
                 mutated_inputlist.append(mutated_string)
                 mutated_string = ""
             
-        return mutated_inputlist
+            return mutated_inputlist
 
 
 
-    def flip_two(length, option):
+    def flip_two(length, option, string):
+        #print('flip two')
         obj = BitFlip()
 
-        seed_func = obj.select_seed(option)
+        if option == 'none':
+            mutated_inputstring = ""
+            input_list = []
 
-        input_string = ""
-        input_string = seed_func(length)
+            for letter in string:
+                input_list.append(letter)
 
-        input_list = []
-        for letter in input_string:
-            input_list.append(letter)
+            index = randrange(len(input_list))
+            bit1 = 1 << randrange(8)
+            bit2 = 1 << randrange(8)
+            while bit1 == bit2:
+                    bit2 = randint(0,8)
 
-        mutated_inputlist = []
-        mutated_inputlist.append(input_string)
-        for i in range(0, len(input_list)):
-            temp_character = input_list[i]
+            input_list[index] = chr(ord(input_list[index]) ^ bit1)
+            input_list[index] = chr(ord(input_list[index]) ^ bit2)
 
-            bit_1 = randint(0, 8)
-            bit_2 = randint(0,8)
-            while bit_1 == bit_2:
+            mutated_inputstring = ''.join(input_list)
+            
+            return mutated_inputstring
+
+        else:
+            seed_func = obj.select_seed(option)
+
+            input_string = ""
+            input_string = seed_func(length)
+
+            input_list = []
+            for letter in input_string:
+                input_list.append(letter)
+
+            mutated_inputlist = []
+            mutated_inputlist.append(input_string)
+            for i in range(0, len(input_list)):
+                temp_character = input_list[i]
+
+                bit_1 = randint(0, 8)
                 bit_2 = randint(0,8)
-            bit_1 = 1 << bit_1
-            bit_2 = 1 << bit_2
+                while bit_1 == bit_2:
+                    bit_2 = randint(0,8)
+                bit_1 = 1 << bit_1
+                bit_2 = 1 << bit_2
 
-            temp_character = chr(ord(temp_character) ^ bit_1)
-            new_character = chr(ord(temp_character) ^ bit_2)
-            input_list[i] = new_character
+                temp_character = chr(ord(temp_character) ^ bit_1)
+                new_character = chr(ord(temp_character) ^ bit_2)
+                input_list[i] = new_character
 
-            mutated_string = ''.join(input_list)
-            mutated_inputlist.append(mutated_string)
-            mutated_string = ""
+                mutated_string = ''.join(input_list)
+                mutated_inputlist.append(mutated_string)
+                mutated_string = ""
 
-        return mutated_inputlist
+            return mutated_inputlist
 
     
-    def flip_two_restore(length, option):
+    def flip_two_restore(length, option, string):
         obj = BitFlip()
 
         seed_func = obj.select_seed(option)
